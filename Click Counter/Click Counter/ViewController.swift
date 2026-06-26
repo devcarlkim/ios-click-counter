cc56b640f94b428bb7dc6cf0e7846d35c420e47d
Carlsan <carlsankim.dev@gmail.com>
2026-06-26 17:07:05 +0800

    Add initial click counter template

---
+++ Click Counter/Click Counter/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Carlsan Kim on 6/26/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        
+        //Buttons
+        let button = UIButton()
+        button.frame = CGRect(x:150, y:250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
+        
+    }
+}
+
